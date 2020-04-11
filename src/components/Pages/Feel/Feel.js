9d49e9c22aadd44e09192e3d6182a6bd51cf2196
Jordan Walker <jwalker117@gmail.com>
2020-04-11 16:30:06 -0500

    Connected redux store to feel page and created reducer

--- src/components/Pages/Feel/Feel.js
+++ src/components/Pages/Feel/Feel.js
@@ -3,16 +3,39 @@ import React, { Component } from 'react';
 //Import to do routing
 import {withRouter} from 'react-router-dom';
 
+//Connect to redux store
+import { connect } from 'react-redux';
+
 //Import Styles
 import './Feel.css'
 
 class Feel extends Component {
+    //Local state
+    state = {
+        feelState: {
+            level: '',
+            summary: '',
+        }
+    }
 
-    //Method for changing pages
-    handleChangePage = () => {
-        console.log('handleChangePage for Feel');
-        //change page to info page
-        this.props.history.push( '/understand' );
+    //Method for handling a change in state on the form
+    handleChangeForState = (event, propertyName) => {
+        this.setState({
+            feelState: {
+                ...this.state.feelState,
+                [propertyName]: event.target.value
+            }
+        });
+    }
+
+    //Method for changing a page and submitting feel page state to the redux store
+    handleAnswer = (event) => {
+        event.preventDefault();
+        console.log(`Local State Feel`, this.state.feelState);
+        //Send state to the redux store
+        this.props.dispatch({type: 'SET_FEEL', payload: this.state.feelState});
+        //Change page from / to /understand
+        this.props.history.push('/understand');
     }
 
   render() {
@@ -23,23 +46,26 @@ class Feel extends Component {
                 <div>
                     <h2>How are you feeling today?</h2>
                     <span>I'm very stressed!</span>
-                    <div class="inner-div">
-                        <label className="container">1<input type="radio" value="1" name="Type" /><span class="checkmark"></span></label>
-                        <label className="container">2<input type="radio" value="2" name="Type" /><span class="checkmark"></span></label>
-                        <label className="container">3<input type="radio" value="3" name="Type" /><span class="checkmark"></span></label>
-                        <label className="container">4<input type="radio" value="4" name="Type" /><span class="checkmark"></span></label>
-                        <label className="container">5<input type="radio" value="5" name="Type" /><span class="checkmark"></span></label>
+                    <div className="inner-div" onChange={(event) => this.handleChangeForState(event, 'level')}>
+                        <label className="container">1<input type="radio" value="1" name="Type" /><span className="checkmark"></span></label>
+                        <label className="container">2<input type="radio" value="2" name="Type" /><span className="checkmark"></span></label>
+                        <label className="container">3<input type="radio" value="3" name="Type" /><span className="checkmark"></span></label>
+                        <label className="container">4<input type="radio" value="4" name="Type" /><span className="checkmark"></span></label>
+                        <label className="container">5<input type="radio" value="5" name="Type" /><span className="checkmark"></span></label>
                     </div>
                     <span>I'm feeling great!</span>
                 </div>
                 
                 {/* Use conditional Rendering to decide once a radio button is selected to display and the type of message */}
                 <div>
-                    <textarea placeholder="Conditional Rendering for Placeholder based of radio button picked">
+                    <textarea 
+                        placeholder="Conditional Rendering for Placeholder based of radio button picked"
+                        onChange={(event) => this.handleChangeForState(event, 'summary')}
+                        value={this.state.summary}>
                     </textarea>
                 </div>
             </section>
-            <button className="button" onClick={this.handleChangePage}>Next</button>
+            <button className="button" onClick={this.handleAnswer}>Next</button>
         </form>
         
         </div>
@@ -47,4 +73,4 @@ class Feel extends Component {
   }
 }
 
-export default withRouter(Feel);
+export default withRouter(connect()(Feel));
--- src/index.js
+++ src/index.js
@@ -4,5 +4,27 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Bring in Redux
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+//Bring in Redux Logger
+import logger from 'redux-logger';
+
+//Add reducers to create global state
+const feel = (state = {}, action) => {
+    if(action.type === 'SET_FEEL') {
+        console.log('Redux State Feel', action.payload);
+        return action.payload;
+    }
+    return state;
+}
+
+// The store is the big JavaScript Object that holds all of the information for our application
+const storeInstance = createStore(
+    combineReducers({feel}), //Add reducer functions to combineReducers
+    applyMiddleware(logger) //Add our middleware logger
+);
+
+// Wrap our App in a Provider, this makes Redux available in our entire application
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
