86959260a2876c5ebbd7ed129f69e289bdc25980
Jordan Walker <jwalker117@gmail.com>
2020-04-11 15:35:45 -0500

    Styling for radio button and text area on Feel page

--- src/components/Pages/Feel/Feel.js
+++ src/components/Pages/Feel/Feel.js
@@ -3,6 +3,9 @@ import React, { Component } from 'react';
 //Import to do routing
 import {withRouter} from 'react-router-dom';
 
+//Import Styles
+import './Feel.css'
+
 class Feel extends Component {
 
     //Method for changing pages
@@ -14,18 +17,31 @@ class Feel extends Component {
 
   render() {
     return (
-        <div>
-        <h2>How are you feeling today?</h2>
-        <span>I'm very stressed!</span>
-        <label><input type="radio" value="1" name="Type" />1</label>
-        <label><input type="radio" value="2" name="Type" />2</label>
-        <label><input type="radio" value="3" name="Type" />3</label>
-        <label><input type="radio" value="4" name="Type" />4</label>
-        <label><input type="radio" value="5" name="Type" />5</label>
-        <span>I'm feeling great!</span>
-        <textarea placeholder="Conditional Rendering for Placeholder based of radio button picked">
-        </textarea>
-        <button onClick={this.handleChangePage}>Next</button>
+        <div className="top-div">
+        <form>
+            <section>
+                <div>
+                    <h2>How are you feeling today?</h2>
+                    <span>I'm very stressed!</span>
+                    <div class="inner-div">
+                        <label className="container">1<input type="radio" value="1" name="Type" /><span class="checkmark"></span></label>
+                        <label className="container">2<input type="radio" value="2" name="Type" /><span class="checkmark"></span></label>
+                        <label className="container">3<input type="radio" value="3" name="Type" /><span class="checkmark"></span></label>
+                        <label className="container">4<input type="radio" value="4" name="Type" /><span class="checkmark"></span></label>
+                        <label className="container">5<input type="radio" value="5" name="Type" /><span class="checkmark"></span></label>
+                    </div>
+                    <span>I'm feeling great!</span>
+                </div>
+                
+                {/* Use conditional Rendering to decide once a radio button is selected to display and the type of message */}
+                <div>
+                    <textarea placeholder="Conditional Rendering for Placeholder based of radio button picked">
+                    </textarea>
+                </div>
+            </section>
+            <button className="button" onClick={this.handleChangePage}>Next</button>
+        </form>
+        
         </div>
     );
   }
